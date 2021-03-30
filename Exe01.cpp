972ae996c9e1c83fbe6421efab90c151e6f328cd
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-03-30 00:48:01 -0300

    Add files via upload

---
+++ Exe01.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <list>
+#include <queue>
+
+using namespace std;
+int main() {
+	
+  int n;
+ cout << " Digite  o  tamanho  do  arranjo : ";
+  cin >> n;
+
+ int *v;
+
+ v = new int[n];
+ for ( int i = 0; i < n; i++) {
+ 	
+ cin >> v[i];
+ }
+ int chave , posicao = 0;
+  {
+ cout << " Digite  o  elemento  que  deseja   buscar : ";
+ cin >> chave ;
+  if ( posicao == -1){
+ cout << "O  elemento  não  existe " << endl ;
+ } else {
+cout << " Elemento  na  posic ão v[" << posicao <<
+"]" << endl ;
+ }
+ } while ( chave != -1);
+ return 0;
+ }
+ 
+  int buscasequencial ( int a[], int n, int x)
+ {
+ for ( int i = 0; i<n; i++){
+ if (a[i]== x){
+ return i;
+ }
+ }
+ return -1;
+ }
+
+
+ 
+ 
+
