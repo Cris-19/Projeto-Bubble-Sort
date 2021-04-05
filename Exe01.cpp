b6500baa1dcd72bfd1a6f1f74c588cb181c9a515
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-04-05 09:41:45 -0300

    Add files via upload

--- Exe01.cpp
+++ Exe01.cpp
@@ -6,27 +6,28 @@ using namespace std;
 int main() {
 	
   int n;
+ int x [10];
  cout << " Digite  o  tamanho  do  arranjo : ";
+ cout<< "10";
   cin >> n;
-
  int *v;
-
  v = new int[n];
  for ( int i = 0; i < n; i++) {
- 	
  cin >> v[i];
  }
  int chave , posicao = 0;
   {
  cout << " Digite  o  elemento  que  deseja   buscar : ";
+ cout << "12" ;
  cin >> chave ;
-  if ( posicao == -1){
+  if ( posicao = 1){
  cout << "O  elemento  não  existe " << endl ;
  } else {
 cout << " Elemento  na  posic ão v[" << posicao <<
 "]" << endl ;
  }
  } while ( chave != -1);
+ 
  return 0;
  }
  
