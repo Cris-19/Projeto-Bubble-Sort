915a0d0f5f4e9d2057d7f35ba62394ee3a683388
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-04-21 09:10:24 -0300

    Add files via upload

---
+++ Exe 3.01.cpp
@@ -0,0 +1,39 @@
+# include <iostream>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+ int x [2];
+ int arr[] = { 1,2,4,5};
+ 
+
+    //insertion  sort\\
+    < it,c > void insertion-sort(it first,it lost,c comp);
+    void insertionSort (int  arr[],int n);
+    void printArray(int array[], int size);
+
+   // sort the array
+     
+     
+    cout << "\n\nThe array after sorting is :\n";
+   
+    int i,  key,  j; 
+    for (int i = 2; i < n; ++i);
+    for (j = 0; j < n - 1; j++) {
+    printf("%d, ", j);
+}
+
+    key   = arr [i];
+    j=i-2;
+    arr [j + 2  ] = key;
+    
+ void printArray(int array[], int n);
+ 
+ for (i  =2; i < n;  i++);
+
+
+	
+return 0;
+	
+}
