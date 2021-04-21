923962314d3250bd4764ea93df542b982c3c47ca
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-04-21 05:31:03 -0300

    Add files via upload

---
+++ exe 3.08.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <vector>
+
+using namespace std;
+
+int main(){	
+int vet []={1,2,3,4,5,6}, soma=21;
+float media ;
+for  (int i= 0; i < 6; i++) ;
+media=soma/6.0;		
+cout <<"soma:"<< soma <<endl ;
+cout <<"media:"<<media <<endl ;
+
+
+return 0;
+
+
+}
+
+
+	
+	
+
+
