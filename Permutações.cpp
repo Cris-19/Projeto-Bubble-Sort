4e3d0ba19f8d52a2c51aee0315f4797d7dbf123b
Cris-19 <81449781+Cris-19@users.noreply.github.com>
2021-04-25 17:04:28 -0300

    Add files via upload

---
+++ Permutações.cpp
@@ -0,0 +1,32 @@
+ #include <stdio.h>
+ #include <iostream>
+ #include <vector>
+using namespace std;
+
+int main(){
+	
+ void funcao () ;	
+ void permuta(int vetor[], int inf, int sup);	
+  printf("\n");
+ int main (int argc, char *argv[]);
+ int v[] = {1, 2, 3, 4};
+ int tam_v = sizeof(v) / sizeof(int);
+ void swap(char* a, char* b);
+ void rev(string& s, int l, int r);
+ int bsearch(string& s, int l, int r, int key);
+ int index = -1;
+ int myints[] = {1,2,3};
+ std::cout << "As 3! Permutações possíveis com 3 elementos: \ n";
+ std::cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+ std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+  std::cout << "1,2,3 " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+
+	return 0;
+	
+	
+}
+
+
+
+
+
